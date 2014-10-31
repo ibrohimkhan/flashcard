6029dbd9a07ab17cbabdad890675c71846a4c07a
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2014-11-01 02:15:04 +0500

    defined new date formater method to parse date in index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_card_date(date)
+    day = date.day <= 9 ? ('0' + date.day.to_s) : date.day.to_s
+    month = date.month <= 9 ? ('0' + date.month.to_s) : date.month.to_s
+    year = date.year.to_s
+
+    day + '/' + month + '/' + year
+  end
 end
