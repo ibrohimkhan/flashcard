53a6785d46ed24c52747e4fadd5e33ef197fd684
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2014-11-01 20:16:05 +0500

    removed method in helper and used strftime method to parse date in view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def format_card_date(date)
-    day   = date.day.to_s.rjust(2, '0')
-    month = date.month.to_s.rjust(2, '0')
-    year  = date.year.to_s
-
-    [day, month, year].join('/')
-  end
 end
