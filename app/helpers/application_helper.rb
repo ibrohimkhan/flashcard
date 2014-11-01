33c2af3afac7d6645e140647ae3f92dc480284cf
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2014-11-01 14:20:20 +0500

    added new methods to format date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def format_card_date(date)
-    day = date.day <= 9 ? ('0' + date.day.to_s) : date.day.to_s
-    month = date.month <= 9 ? ('0' + date.month.to_s) : date.month.to_s
+    day = date.day <= 9 ? date.day.to_s.rjust(2, '0') : date.day.to_s
+    month = date.month <= 9 ? date.month.to_s.rjust(2, '0') : date.month.to_s
     year = date.year.to_s
 
-    day + '/' + month + '/' + year
+    [day, month, year].join('/')
   end
 end
