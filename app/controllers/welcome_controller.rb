8eeac85c9479f68346dc89455ff1d281950d9079
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2014-11-06 18:34:46 +0500

    designed card UI in welcome page. Created scopes in card model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @card = Card.first_card(Date.today)
+    @review = Card.new
+    @review.original_text = @card.original_text
+    #@next = Card.next_card(Date.today, @card)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,5 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true
+  scope :first_card, -> (date) { where("review_date <= ?", date).first }
+  scope :next_card, -> (date, card) { where("review_date <= ? and id > ?", date, card.id).first }
 end
