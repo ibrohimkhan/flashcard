40e44567caf12ad4fe8bcdd729a1d04867a92787
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2014-11-07 23:13:07 +0500

    code style formatted

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,7 +4,6 @@ class Card < ActiveRecord::Base
 
   scope :first_card, -> (date) { where("review_date <= ?", date).first }
   scope :next_card, -> (date, card_id) { where("review_date <= ? and id > ?", date, card_id).first }
-  scope :find_translation, -> (original_text, translated_text) {
-    where("original_text = ? and translated_text = ?", original_text, translated_text) }
-
+  scope :find_translation, -> (original_text, translated_text) { where("original_text = ? and translated_text = ?",
+                                                                       original_text, translated_text) }
 end
